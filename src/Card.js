256e9ea614adc2bb45c9ebbb0f8a20f54a3d2f17
Luciano Simoni <lucianosimonipersonal@gmail.com>
2022-11-17 00:40:09 +0000

    Prettier Button added

--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,9 @@
 import "./Card.css";
 
 function Card({ spriteName, seed }) {
+  const capitalizedName =
+    spriteName.charAt(0).toUpperCase() + spriteName.slice(1);
+
   return (
     <div className="card">
       <div className="image">
@@ -8,6 +11,11 @@ function Card({ spriteName, seed }) {
           alt="Sprite"
           src={`https://avatars.dicebear.com/api/${spriteName}/${seed}.svg`}
         ></img>
+      </div>
+
+      <div className="info">
+        <h2>{capitalizedName}</h2>
+        <button>Select</button>
       </div>
     </div>
   );
